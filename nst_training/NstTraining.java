28806ddf0683908505a2d4bb12fcc32a3a8e1909
Eric Spishak-Thomas <emspishak@gmail.com>
2022-05-14 16:50:29 -0400

    Add data about lawsuits, complaint docs, and officer docs.

--- nst_training/NstTraining.java
+++ nst_training/NstTraining.java
@@ -1,5 +1,7 @@
 package emspishak.nypd.nsttraining;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.opencsv.CSVWriter;
 import java.io.File;
@@ -28,9 +30,14 @@ public final class NstTraining {
     "srg_trainings",
     "50a_link",
     "nypd_profile_link",
+    "lawsuits_count",
+    "complaint_docs",
+    "officer_docs"
   };
 
   private static final DateTimeFormatter INPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("M/d/u");
+
+  private static final Joiner NEW_LINE = Joiner.on('\n');
 
   @Option(name = "-profile-dir", usage = "Directory with NYPD profile JSON.")
   private File profileDir;
@@ -49,7 +56,9 @@ public final class NstTraining {
     CmdLineParser parser = new CmdLineParser(this);
     parser.parseArgument(args);
 
-    ImmutableMap<Integer, JSONObject> taxIds = get50AData();
+    JSONObject json = new JSONObject(Files.readString(fiftyAData.toPath()));
+    ImmutableMap<Integer, JSONObject> taxIds = get50AData(json);
+    ImmutableMap<String, JSONObject> complaints = get50AComplaints(json);
 
     CSVWriter writer = new CSVWriter(new FileWriter(outputFile));
     writer.writeNext(OUTPUT_HEADERS);
@@ -57,10 +66,10 @@ public final class NstTraining {
     for (char c = 'A'; c <= 'Z'; c++) {
       File jsonFile = new File(profileDir, String.format("nypd-profiles-%s.json", c));
       String contents = Files.readString(jsonFile.toPath());
-      JSONArray json = new JSONArray(contents);
+      JSONArray profiles = new JSONArray(contents);
 
-      for (int i = 0; i < json.length(); i++) {
-        JSONObject profile = json.getJSONObject(i);
+      for (int i = 0; i < profiles.length(); i++) {
+        JSONObject profile = profiles.getJSONObject(i);
         JSONArray training;
         try {
           training = profile.getJSONObject("reports").getJSONArray("training");
@@ -92,7 +101,10 @@ public final class NstTraining {
                     ? ""
                     : String.format(
                         "https://www.50-a.org/officer/%s", matchedData.getString("unique_mos")),
-                String.format("https://oip.nypdonline.org/view/1/@TAXID=%s", taxId)
+                String.format("https://oip.nypdonline.org/view/1/@TAXID=%s", taxId),
+                getLawsuitsCount(matchedData),
+                NEW_LINE.join(getComplaintDocuments(matchedData, complaints)),
+                NEW_LINE.join(getOfficerDocuments(matchedData))
               };
           writer.writeNext(row);
         }
@@ -114,13 +126,12 @@ public final class NstTraining {
   }
 
   /** Maps from tax ID to 50a data blob. */
-  private ImmutableMap<Integer, JSONObject> get50AData() throws IOException {
-    String contents = Files.readString(fiftyAData.toPath());
-    JSONObject json = new JSONObject(contents).getJSONObject("officers");
+  private ImmutableMap<Integer, JSONObject> get50AData(JSONObject json) throws IOException {
+    JSONObject officers = json.getJSONObject("officers");
 
     ImmutableMap.Builder<Integer, JSONObject> data = ImmutableMap.builder();
-    for (String mos : json.keySet()) {
-      JSONObject profile = json.getJSONObject(mos);
+    for (String mos : officers.keySet()) {
+      JSONObject profile = officers.getJSONObject(mos);
       int taxId = profile.optInt("taxid");
       if (taxId > 0) {
         data.put(taxId, profile);
@@ -128,5 +139,69 @@ public final class NstTraining {
     }
 
     return data.build();
+  }
+
+  private ImmutableMap<String, JSONObject> get50AComplaints(JSONObject json) throws IOException {
+    JSONObject complaints = json.getJSONObject("complaints");
+
+    ImmutableMap.Builder<String, JSONObject> data = ImmutableMap.builder();
+    for (String id : complaints.keySet()) {
+      JSONObject complaint = complaints.getJSONObject(id);
+      data.put(id, complaint);
+    }
+
+    return data.build();
+  }
+
+  private ImmutableList<String> getComplaintDocuments(
+      JSONObject officer, ImmutableMap<String, JSONObject> allComplaints) {
+    if (officer == null) {
+      return ImmutableList.of();
+    }
+
+    ImmutableList.Builder<String> docs = ImmutableList.builder();
+
+    for (Object id : officer.getJSONArray("complaints")) {
+      JSONObject complaint = allComplaints.get((String) id);
+      if (complaint.has("resources")) {
+        JSONArray complaints = complaint.getJSONObject("resources").getJSONArray("complaints");
+        for (Object detailsObj : complaints) {
+          JSONObject details = (JSONObject) detailsObj;
+          String url = details.getString("url");
+          if (url.startsWith("/")) {
+            url = String.format("https://50-a.org%s", url);
+          }
+          docs.add(url);
+        }
+      }
+    }
+
+    return docs.build();
+  }
+
+  private String getLawsuitsCount(JSONObject officer) {
+    if (officer == null) {
+      return "";
+    }
+
+    if (officer.has("lawsuits")) {
+      return Integer.toString(officer.getJSONObject("lawsuits").getJSONArray("cases").length());
+    }
+    return "";
+  }
+
+  private ImmutableList<String> getOfficerDocuments(JSONObject officer) {
+    if (officer == null) {
+      return ImmutableList.of();
+    }
+
+    ImmutableList.Builder<String> docs = ImmutableList.builder();
+    if (officer.has("documents")) {
+      JSONArray jsonDocuments = officer.getJSONArray("documents");
+      for (int i = 0; i < jsonDocuments.length(); i++) {
+        docs.add(jsonDocuments.getJSONObject(i).getString("url"));
+      }
+    }
+    return docs.build();
   }
 }
